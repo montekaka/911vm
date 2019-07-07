620795689ae323053acdc9aed8ab1c579e8bb411
montekaka <josh.chen.jia@gmail.com>
2019-07-06 23:02:50 -0700

    add driver training course model

---
+++ database/models/driver_training_course.js
@@ -0,0 +1,10 @@
+const DriverTrainingCourse = {
+    id: {
+      primary: true,
+      type: 'uuid',
+      required: true,
+    },
+    name: 'string'    
+  }
+  
+  module.exports = DriverTrainingCourse;
