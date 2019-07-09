de7789cdc6655e5e39e5d3357e766c689b07ec95
montekaka <josh.chen.jia@gmail.com>
2019-07-08 23:51:37 -0700

    add bodyParser.urlencoded

--- server/express.js
+++ server/express.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 require('./routes.js')(app);
 
