1005e27efba88d9af33004ee7a9ab6676c8f98f0
montekaka <josh.chen.jia@gmail.com>
2019-07-08 21:11:30 -0700

    clean up seed file

--- database/models/libs/seed.js
+++ database/models/libs/seed.js
@@ -10,34 +10,21 @@ const instance = new Neode.fromEnv();
 instance.model('Person', Person);
 instance.model('DriverTrainingCourse', DriverTrainingCourse);
 
-// ['volunteers.csv','driver_training.csv']
+const files = [
+  {filename: 'volunteers.csv', helpername: 'volunteers'},
+  {filename: 'driver_training.csv', helpername: 'driver_training'}
+];
 
-importCSV('volunteers.csv', (queries) => {
-  instance.batch(queryBuilder['volunteers'](queries));
-});
-
-importCSV('driver_training.csv', (queries) => {
-  instance.batch(queryBuilder['driver_training'](queries));
-});
-
-// instance.close();
-
-// MATCH (person:Person {first_name: 'Todd', last_name: 'Baker'}), 
-// (course:DriverTrainingCourse {name: 'Written Test'})
-// CREATE (person)-[:DRIVERTRAINING{received:"3/16/2018"}]->(course)
-
-// MATCH (person:Person {first_name: 'Cody', last_name: 'Daig'}), 
-// (course:DriverTrainingCourse {name: 'Light Cones'})
-// CREATE (person)-[:DRIVERTRAINING{received:"1/28/2020"}]->(course)
-
-// instance.create('DriverTrainingCourse', {
-//   name: 'Written Test'
-// })
-
-// instance.first('Person', 'email_address', 'todd.baker@leu-rescue.org')
-// .then((person) => {
-//   return instance.first('DriverTrainingCourse','name', 'Written Test')
-//   .then((exam) => {
-//     return person.relateTo(exam, 'driverTraining', {received: new Date(2018, 2, 16)})
-//   })
-// })
+Promise.map(files, (file) => {
+  importCSV(file.filename, (queries) => {
+    instance.batch(queryBuilder[file.helpername](queries));
+  });
+}, {
+  concurrency: 2
+})
+.then(() => {
+  console.log('done');
+})
+.catch((err) => {
+  console.log(err);
+})
