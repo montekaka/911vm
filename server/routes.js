874e8713d79e04a88ead6f05c38608b7d8736ab7
montekaka <josh.chen.jia@gmail.com>
2019-07-08 23:51:20 -0700

    add person get and post endpoint

--- server/routes.js
+++ server/routes.js
@@ -3,7 +3,7 @@
 const controllers = require('./controllers.js');
 
 module.exports = function(app) {
-
   app.get('/helloworld', controllers.helloWorld);
-
+  app.get('/v1/person', controllers.person.get);
+  app.post('/v1/person', controllers.person.post);
 };
