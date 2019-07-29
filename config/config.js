d7b59b8111076d7ff27c28bf571f3b8a03d8aea9
montekaka <josh.chen.jia@gmail.com>
2019-07-28 23:50:09 -0700

    adds jwt config

--- config/config.js
+++ config/config.js
@@ -1,5 +1,10 @@
+require('dotenv').config()
 const config = {};
 
 config.port = process.env.PORT || 3030;
+config.secrets = {
+  jwt: process.env.JWT_SECRET,
+  jwtExp: process.env.JWT_EXP
+}
 
 module.exports = config;
